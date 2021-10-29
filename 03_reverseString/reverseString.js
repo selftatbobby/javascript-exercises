df3f72df4dad60c49c1eca987f3a1e79251186fb
Bobby Tat <messaroundacc1@hotmail.com>
2021-10-29 11:12:51 -0700

    completed exercise. did not look at solution. used for loop

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
+let new_string = ''
 const reverseString = function(str) {
-
+    for (i=1; i<=str.length; i++){
+        new_string += str[str.length-i];
+    }
+    return new_string;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+   
