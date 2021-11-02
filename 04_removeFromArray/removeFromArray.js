173db41a8773e9feb69cc0b074fffcbf6c8f564d
Bobby Tat <messaroundacc1@hotmail.com>
2021-11-02 09:20:07 -0700

    passed several test w/o looking at solution. Looked at solution, comprehended the logic to complete exercise.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function (...args) {
+    // the first item in the list of arguments is the array, pulled out with args[0]
+    const array = args[0];
+    // create a new empty array
+    const newArray = [];
+    // use forEach to go through the array
+    array.forEach((item) => {
+      // add every element into newly created array
+        // UNLESS an element is included/equals a function argument
+      // newly created array has every item of original array, 
+        //except those items that included/equals a function argument
+      if (!args.includes(item)) {
+        newArray.push(item);
+      }
+    });
+    //return newly created array
+    return newArray;
+  };
 
 // Do not edit below this line
 module.exports = removeFromArray;
