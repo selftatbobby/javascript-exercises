1882cc72ecf0877a2118af0de6bdd5e371e8c7d1
Bobby Tat <messaroundacc1@hotmail.com>
2021-11-03 10:20:41 -0700

    completed exercise without looking at solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+let calculatedCelsius = 0;
+let calculatedFarenheit  = 0;
+const ftoc = function(farenheit) {
+  calculatedCelsius = (farenheit-32)*(5/9);
+  return Math.round(calculatedCelsius * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  calculatedFarenheit = (celsius * (9/5)) + 32;
+  return Math.round(calculatedFarenheit * 10) / 10;
 };
 
 // Do not edit below this line
