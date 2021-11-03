4bd700f952e4155340ff4e6b2eac0e1dbb7c6c61
Bobby Tat <messaroundacc1@hotmail.com>
2021-11-03 09:52:22 -0700

    looked at solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,7 @@
-const leapYears = function() {
-
-};
+const leapYears = function(year){
+    return year%4===0 && (year%100!==0 || year%400===0);
+    //true if year divisible by 4 AND year not divisible by 100 or year divisibly by 400. 
+  };
 
 // Do not edit below this line
 module.exports = leapYears;
